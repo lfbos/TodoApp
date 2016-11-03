3c517b277650a9a3b58f6ff3370a668763f2cee2
Luis Boscan <luife642@gmail.com>
2016-11-02 21:30:55 -0400

    SearchText functionality added

--- app/tests/api/TodoAPI.test.jsx
+++ app/tests/api/TodoAPI.test.jsx
@@ -51,4 +51,49 @@ describe('TodoAPI', () => {
    expect(actualTodos).toEqual(todos);
   });
  });
+
+ describe('filterTodos', () => {
+  var todos = [{
+   id: 1,
+   text: 'Some text here',
+   completed: true
+  },
+  {
+   id: 2,
+   text: 'Other text here',
+   completed: false
+  },
+  {
+   id: 3,
+   text: 'Some text',
+   completed: true
+  }];
+
+  it('should return all items if showCompleted is true', () => {
+   var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+   expect(filteredTodos.length).toBe(3);
+  });
+
+  it('should return non-completed todos when showCompleted is false', () => {
+   var filteredTodos = TodoAPI.filterTodos(todos, false, '');
+   expect(filteredTodos.length).toBe(1);
+  });
+
+  it('should sort by completed status', () => {
+   var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+   expect(filteredTodos[0].completed).toBe(false);
+   expect(filteredTodos[1].completed).toBe(true);
+   expect(filteredTodos[2].completed).toBe(true);
+  });
+
+  it('should filter todos by searchText', () => {
+   var filteredTodos = TodoAPI.filterTodos(todos, true, 'some');
+   expect(filteredTodos.length).toBe(2);
+  });
+
+  it('should return all todos if searchText is empty', () => {
+   var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+   expect(filteredTodos.length).toBe(3);
+  });
+ });
 });
