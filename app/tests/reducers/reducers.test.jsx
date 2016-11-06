6602b29688a8750b20a36aa253fad4ba4c71f6af
Luis Boscan <luife642@gmail.com>
2016-11-06 11:56:49 -0400

    reducer for todos added

--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -15,7 +15,7 @@ describe('Reducers', () => {
    expect(res).toEqual(action.searchText);
   });
  });
- describe('toggleShowCompleted', () => {
+ describe('toggleShowCompletedReducer', () => {
   it('should toggle showCompleted', () => {
    var action = {
     type: 'TOGGLE_SHOW_COMPLETED'
@@ -24,6 +24,38 @@ describe('Reducers', () => {
    var res = reducers.showCompletedReducer(df(false), df(action));
 
    expect(res).toEqual(true);
+  });
+ });
+
+ describe('todosReducer', () => {
+  it('should add new todo', () => {
+   var action = {
+    type: 'ADD_TODO',
+    text: 'Wash the dishes'
+   };
+   var res = reducers.todosReducer([], action);
+
+   expect(res.length).toEqual(1);
+   expect(res[0].text).toEqual(action.text);
+  });
+
+  it('should toggle todo', () => {
+   var todos = [{
+    id: 1,
+    text: 'Something to do',
+    completed: true,
+    createdAt: 123,
+    completedAt: 125
+   }];
+   var action = {
+    type: 'TOGGLE_TODO',
+    id: 1
+   };
+
+   var res = reducers.todosReducer(df(todos), df(action));
+
+   expect(res[0].completed).toEqual(false);
+   expect(res[0].completedAt).toEqual(undefined);
   });
  });
 });
